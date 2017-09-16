0b64d959b43cf125a07ff105f6a9d96d2beda711
Philippe Plante <plantephilippe@gmail.com>
2017-09-16 16:21:53 -0400

    Wrote Main

--- KataFizzBuzz/src/main/java/ca/leamonde/Main.java
+++ KataFizzBuzz/src/main/java/ca/leamonde/Main.java
@@ -3,5 +3,12 @@ package ca.leamonde;
 public class Main {
     public static void main(String [ ] args)
     {
+        FizzBuzz fb = new FizzBuzz();
+        fb.setCount(1);
+        for(int i = fb.getCount(); i <= 100; i++)
+        {
+            fb.setCount(i);
+            System.out.println(fb.output());
+        }
     }
 }
